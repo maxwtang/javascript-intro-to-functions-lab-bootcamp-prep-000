78009b0603153a137e07b293f8143404cd0c7e7f
tangerwangerrr@gmail.com Tang <tangerwangerrr@gmail.com>
2018-02-22 09:09:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-function shout(string)
+function shout(string) {
+  return string.toUpperCase;
+}
+
+function whisper(string) {
+  return string.toLowerCase;
+}
+
+function logShout(string) {
+  console.log(string);
+}
+
+function logWhisper(string) {
+  console.log(string.toLowerCase);
+}
+
+function sayHiToGrandma(string) {
+  if string === string.toLowerCase ()
+}
