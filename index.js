3f7d085ade25efee7b11cf5fd85efbb594901401
tangerwangerrr@gmail.com Tang <tangerwangerrr@gmail.com>
2018-02-22 09:16:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 function shout(string) {
-  return string.toUpperCase;
+  return string.toUpperCase();
 }
 
 function whisper(string) {
-  return string.toLowerCase;
+  return string.toLowerCase();
 }
 
 function logShout(string) {
@@ -11,17 +11,17 @@ function logShout(string) {
 }
 
 function logWhisper(string) {
-  console.log(string.toLowerCase);
+  console.log(string.toLowerCase());
 }
 
 function sayHiToGrandma(string) {
   if (string === "I love you, Grandma.") {
     return "I love you, too.";
   }
-  else if (string === string.toLowerCase) {
+  else if (string === string.toLowerCase()) {
     return "I can't hear you!";
   }
-  else if (string === string.toUpperCase) {
+  else if (string === string.toUpperCase()) {
     return "YES INDEED!";
   }
   else {
